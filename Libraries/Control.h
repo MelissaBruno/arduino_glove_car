4bc31a153f52bc7bf4a35ef5b336ffd95e7a3ce3
Melissa Bruno <brunomr@miamioh.edu>
2017-04-25 20:04:21 -0400

    Add files via upload

---
+++ Libraries/Control.h
@@ -0,0 +1,16 @@
+#ifndef Control_h
+#define Control_h
+
+class ControlClass{
+  public:
+    ControlClass();
+    void SETUP();
+    void forward();
+    void turnLeft();
+    void turnRight();
+    void release();
+};
+
+extern ControlClass control;
+
+#endif
