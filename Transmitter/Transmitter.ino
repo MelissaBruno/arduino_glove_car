254451ac77c74f3da313a0461dfa7b3a2c8de773
Melissa Bruno <brunomr@miamioh.edu>
2017-05-03 12:22:11 -0400

    Update Transmitter.ino

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,14 +1,13 @@
 #include <VirtualWire.h>
-char *controller;         //Char being transmitted
+char const* controller;         //Array that is to be transmitted
 
 int switch6 = 6;
 int switch7 = 7;
 int switch8 = 8;
 
 void setup() {
-   vw_set_ptt_inverted(true);
    vw_set_tx_pin(12);
-   vw_setup(4000);// speed of data transfer Kbps
+   vw_setup(4000);
 
    pinMode(6, INPUT);
    pinMode(7, INPUT);
@@ -19,21 +18,21 @@ void loop() {
 
    switch6 = digitalRead(6);
    switch7 = digitalRead(7);
-   switch8 = digitalRead(7);
+   switch8 = digitalRead(8);
 
    if(switch6 == HIGH){
      controller="0" ;
      vw_send((uint8_t *)controller, strlen(controller));
-     vw_wait_tx();
+     vw_wait_tx();           //waiting for the whole message to be sent
    }
    if(switch7 == HIGH){
      controller="1" ;
      vw_send((uint8_t *)controller, strlen(controller));
-     vw_wait_tx(); // Wait until the whole message is gone
+     vw_wait_tx();
    }
    if(switch8 == HIGH){
      controller="2" ;
      vw_send((uint8_t *)controller, strlen(controller));
-     vw_wait_tx(); // Wait until the whole message is gone
+     vw_wait_tx(); //
    }
 }
