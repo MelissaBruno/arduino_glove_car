57c42c811b6fbbad0df02960d71e1b9580f42595
Melissa Bruno <brunomr@miamioh.edu>
2017-05-03 01:39:30 -0400

    Add files via upload

---
+++ Reciever/Libraries/Control.cpp
@@ -0,0 +1,50 @@
+/*
+ * Library for setting up and controlling the car
+ */
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "Control.h"
+
+Adafruit_MotorShield motor1 = Adafruit_MotorShield();
+
+Adafruit_DCMotor *leftMotor = motor1.getMotor(2);
+Adafruit_DCMotor *rightMotor = motor1.getMotor(1);
+
+ControlClass::ControlClass(){
+}
+
+void ControlClass::SETUP(){
+  motor1.begin();
+  leftMotor->setSpeed(175);
+  rightMotor->setSpeed(175); 
+}
+
+/*
+ * Returns position of cursor
+ */
+void ControlClass::forward(){
+  leftMotor->run(FORWARD);
+  rightMotor->run(FORWARD);
+}
+
+void ControlClass::backward(){
+  leftMotor->run(BACKWARD);
+  rightMotor->run(BACKWARD);
+}
+
+void ControlClass::turnLeft(){
+  leftMotor->run(RELEASE);
+  rightMotor->run(FORWARD);
+}
+
+void ControlClass::turnRight(){
+  leftMotor->run(FORWARD);
+  rightMotor->run(RELEASE);
+}
+
+void ControlClass::release(){
+  leftMotor->run(RELEASE);
+  rightMotor->run(RELEASE);
+}
+
