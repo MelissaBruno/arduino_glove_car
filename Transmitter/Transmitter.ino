bd171b935e4e08d8bcb00cfe6d400055713b3e13
Melissa Bruno <brunomr@miamioh.edu>
2017-05-05 21:09:43 -0400

    Update Transmitter.ino

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,38 +1,48 @@
+/*
+ * Class for transmitting commands based on the values of flex sensors.
+ */
 #include <VirtualWire.h>
-char const* controller;         //Array that is to be transmitted
+char const* controller;
 
-int switch6 = 6;
-int switch7 = 7;
-int switch8 = 8;
+int flexval = 0;
+int turnval = 0;
 
 void setup() {
-   vw_set_tx_pin(12);
-   vw_setup(4000);
+   vw_set_tx_pin(12);      //Transmitter pin
+   vw_setup(4000);         //Initialized the library, bits/sec
 
-   pinMode(6, INPUT);
-   pinMode(7, INPUT);
-   pinMode(8, INPUT);
+   Serial.begin(9600);
 }
 
 void loop() {
 
-   switch6 = digitalRead(6);
-   switch7 = digitalRead(7);
-   switch8 = digitalRead(8);
-
-   if(switch6 == HIGH){
-     controller="0" ;
-     vw_send((uint8_t *)controller, strlen(controller));
-     vw_wait_tx();           //waiting for the whole message to be sent
-   }
-   if(switch7 == HIGH){
-     controller="1" ;
+   flexval = analogRead(A5);
+   turnval = analogRead(A4);
+   Serial.println(flexval);
+   
+   /* Different commmands are sent depending on the values of the
+    * flex sensors. vw_wait_tx() waits for the entire message to be sent.
+    */
+   if(flexval<40){
+     controller="0";
      vw_send((uint8_t *)controller, strlen(controller));
      vw_wait_tx();
-   }
-   if(switch8 == HIGH){
-     controller="2" ;
+   }else if(flexval>70){
+     controller="2";
      vw_send((uint8_t *)controller, strlen(controller));
-     vw_wait_tx(); //
+     vw_wait_tx();
+   }else if(turnval < 45){
+      controller="3";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx(); 
+   }else if(turnval > 58){
+      controller="4";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx(); 
+   }else{
+      controller="1";
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx(); 
    }
+   
 }
