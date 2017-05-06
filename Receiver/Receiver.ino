96c2f9266aede49710960bde12522afd6e24d3da
Melissa Bruno <brunomr@miamioh.edu>
2017-05-05 21:15:48 -0400

    Update Receiver.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,3 +1,7 @@
+/*
+ * Class for recieving and moving the car.
+ */
+
 #include <VirtualWire.h>
 #include "Control.h"
 
@@ -10,13 +14,10 @@ long duration;
 int distance;
 
 void setup() {
-
   control.SETUP();
 
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  
-  Serial.begin(9600);
   
   vw_set_rx_pin(2);
   vw_setup(4000);
@@ -24,7 +25,8 @@ void setup() {
 }
 
 void loop() {
-
+  
+  // Monitors the distance of object in front of it
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
 
@@ -32,38 +34,33 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
+  // Reads echoPin, returns the sound wave travel time and calculates distance
   duration = pulseIn(ECHO_PIN, HIGH);
-  // Calculating the distance
   distance= duration*0.034/2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
   
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
   
   if (vw_get_message(buf, &buflen)) {
-    
      if(buf[0]=='0') {
-        if(distance <= 10){
+        if(distance <= 10){     //If distance is less than 6", stop the car
           control.release();  
         }
         else{
-          control.forward();
+          control.forward();    //Forward
         }
      }
      if(buf[0]=='1') {
-        control.release();
+        control.release();      //Stop
      }
      if(buf[0]=='2') {
-        control.backward();
+        control.backward();     //Reverse
      }
      if(buf[0]=='3') {
-        control.turnLeft();
+        control.turnLeft();     //Left turn
      }
      if(buf[0]=='4') {
-        control.turnRight();
+        control.turnRight();    //Right turn
      }
- 
-  } //if
+  }
 } //loop
